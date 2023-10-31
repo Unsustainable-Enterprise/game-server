df9cd68ec3d7baadd887202d7a7328385a648472
Ivan Varava <varavaivan1@gmail.com>
2023-10-31 20:34:55 +0200

    fixed checkForDuplicate

--- src/utils/generatePin.ts
+++ src/utils/generatePin.ts
@@ -1,16 +1,19 @@
+import { LobbyManager } from '../managers/lobbyManager';
+
 export function generatePin() {
     const pin = (Math.floor(Math.random() * 90000) + 10000).toString();
-    // if (!checkForDuplicate(pin)) {
-    //     return generatePin();
-    // }
+    if (!checkForDuplicate(pin)) {
+        return generatePin();
+    }
     return pin;
 }
 
-// function checkForDuplicate(pin: string): boolean {
-//     sessions.forEach((session) => {
-//         if (session.pin === pin) {
-//             return false;
-//         }
-//     });
-//     return true;
-// }
+function checkForDuplicate(pin: string): boolean {
+    const lobby = LobbyManager.findlobbyByPin(pin);
+
+    if (lobby) {
+        return true;
+    }
+
+    return false;
+}
